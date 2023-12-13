b42442764549f9a6b0199f1275f93dc5500f599a
Jhonattan <jhonattanlino@outlook.com>
2023-12-13 00:10:28 -0300

    config: start project

---
+++ tests/target/debug/deps/tests-852e16b3b916a3f1.d
@@ -0,0 +1,5 @@
+/root/Projetos/Rust/first/Rust/tests/target/debug/deps/tests-852e16b3b916a3f1: src/main.rs
+
+/root/Projetos/Rust/first/Rust/tests/target/debug/deps/tests-852e16b3b916a3f1.d: src/main.rs
+
+src/main.rs:
---
+++ tests/target/debug/tests.d
@@ -0,0 +1,1 @@
+/root/Projetos/Rust/first/Rust/tests/target/debug/tests: /root/Projetos/Rust/first/Rust/tests/src/main.rs
