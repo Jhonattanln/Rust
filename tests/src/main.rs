b32f3ef7d0fc214c7e969248c5a0b9f3bcf8f1a3
Jhonattan <jhonattanlino@outlook.com>
2023-12-13 22:55:21 -0300

    feat: tuplas and variables

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,4 +1,11 @@
+ let x = 1;
+ 
  fn main() {
     println!("Hello, world!");
-    println!("The fist number is {}", 1);
+    println!("The fist number is {}", x);
 }
+
+// Tuplas 
+let tupla_e = ('E', 5i32, 5.5f64, true);
+
+println!('Tuplas: {} {} {} {}', tupla_e.0, tupla_e.1, tupla_e.2, tupla_e.3);
