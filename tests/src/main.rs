293026f85405053c81cab1367b5f0074c7206c17
Jhonattan <jhonattanlino@outlook.com>
2023-12-29 23:30:02 -0300

    feat: structs

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,11 +1,29 @@
- let x = 1;
- 
- fn main() {
+const X: i32 = 1;
+
+fn main() {
     println!("Hello, world!");
-    println!("The fist number is {}", x);
+    println!("The first number is {}", X);
+
+    // Tuplas
+    const TUPLA_E: (char, i32, f64, bool) = ('E', 5, 5.5, true);
+    println!("Tuplas: {}, {}, {}, {}", TUPLA_E.0, TUPLA_E.1, TUPLA_E.2, TUPLA_E.3);
+
+    print_function();
 }
 
-// Tuplas 
-let tupla_e = ('E', 5i32, 5.5f64, true);
+// structs
+struct Clubs {
+    club_name: &'static str,
+    birth: i32,
+    bianconeri: f64
+}
 
-println!('Tuplas: {} {} {} {}', tupla_e.0, tupla_e.1, tupla_e.2, tupla_e.3);
+static JUVENTUS: Clubs = Clubs {
+    club_name: "Juventus",
+    birth: 1897,
+    bianconeri: 0.5
+};
+
+fn print_function() {
+    println!("Structs: {}, {}, {}", JUVENTUS.club_name, JUVENTUS.birth, JUVENTUS.bianconeri);
+}
